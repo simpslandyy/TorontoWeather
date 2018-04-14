0d075cd34b52c0caa87b5730227c50ec4706ec0b
simpslandyy <landy.simpson@mail.utoronto.ca>
2018-04-14 06:10:46 -0400

    entrypoint for application, fetching data to store

---
+++ src/app.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import * as reducers from './store/reducer';
+import { TorontoWeather } from './containers/weather';
+import { fetchWeather } from './store/action';
+
+import "regenerator-runtime/runtime"; //async await
+import './_custom.scss';
+import 'bootstrap';
+
+
+const store = createStore(combineReducers(reducers), applyMiddleware(thunk));
+
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    this.props.getWeather();
+  }
+
+  render () {
+    return (<TorontoWeather {...this.props} />)
+    }
+}
+
+const mapDispatchtoProps = dispatch => {
+  return {
+    getWeather: () => {
+      dispatch(fetchWeather('Toronto, ON'));
+    }
+  }
+}
+
+const mapStatetoProps = state => {
+  return {
+    search: 'Toronto, ON'
+  }
+}
+
+
+const WeatherApp = connect(mapStatetoProps, mapDispatchtoProps)(App);
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <WeatherApp />
+  </Provider>,
+  document.getElementById('root')
+);
